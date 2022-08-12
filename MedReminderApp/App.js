7bf1bde439b20bfe1e1c27ba16eea7eae78d5718
atienzisaa <atienzisaa@ecs.vuw.ac.nz>
2022-08-12 22:16:09 +1200

    added list model and imports/exports to show list model in App.js

--- MedReminderApp/App.js
+++ MedReminderApp/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { data } from './src/model/list';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        value: {data[0].items}
+        </Text>
       <StatusBar style="auto" />
     </View>
-    
   );
 }
 
---
+++ MedReminderApp/src/model/list.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        day: "Monday",
+        items: [0, 1, 2]
+    },
+    {
+        day: "Tuesday",
+        items: [3, 4, 5]
+    },
+    {
+        day: "Wednesday",
+        items: [0, 1, 2]
+    },
+    {
+        day: "Thursday",
+        items: [0, 1, 2]
+    },
+    {
+        day: "Friday",
+        items: [0, 1, 2]
+    },
+    {
+        day: "Saturday",
+        items: [0, 1, 2]
+    },
+    {
+        day: "Sunday",
+        items: [0, 1, 2]
+    }
+];
+
+export default {
+    data,
+}
