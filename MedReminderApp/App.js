0404a3f2edbf5275f0526288b2113e559db9e26b
Johanah Gloria <gloriajoha@myvuw.ac.nz>
2022-08-16 17:16:26 +1200

    Created basic nav bar

--- MedReminderApp/App.js
+++ MedReminderApp/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
 import { PillList } from './src/model/list';
 
 export default function App() {
   return (
     <View style={styles.container}>
       
+      {/* Top Nav */}
+      <View style={{backgroundColor : "#A3CEF1", height : "10%", width: "100%"}}>
+        
+        <View style={styles.fixToText}>
+          <Button
+          title="Profile"
+          onPress={() => Alert.alert("insert profile link here")}
+          />
+          
+          <Text style={{color : "#6096BA", fontSize : 30, fontWeight : "bold", textAlign : "center"}}> MedApp </Text>
+
+          <Button
+          title="Sign out"
+          onPress={() => Alert.alert("You are now signed out")}
+          />
+        </View>
+
+      </View>
+        
       <PillList day={"Monday"}/>
       <StatusBar style="auto" />
       
@@ -19,5 +38,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    top: "10%",
   },
 });
