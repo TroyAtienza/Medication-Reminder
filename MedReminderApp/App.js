526aaf7ecae3f9a4eabe7f6c5e7d375ccc396904
atienzisaa <atienzisaa@ecs.vuw.ac.nz>
2022-08-19 10:51:53 +1200

    cleaned imports

--- MedReminderApp/App.js
+++ MedReminderApp/App.js
@@ -1,5 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/00_HomeScreen";
