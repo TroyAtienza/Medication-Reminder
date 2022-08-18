8d5af2c6070edb57ec15b11ac6a7a5be0b64634d
atienzisaa <atienzisaa@ecs.vuw.ac.nz>
2022-08-19 10:38:41 +1200

    added navigation and created blank files

--- MedReminderApp/src/screens/01_PillListScreen.js
+++ MedReminderApp/src/screens/01_PillListScreen.js
@@ -1,27 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { PillList } from '../model/List.js';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import PillListScreen from "./src/screens/01_PillListScreen.js";
-
-const Stack = createNativeStackNavigator();
-
 export default function App() {
-  // TODO: When Home screen is completed change home component to actual home screen.
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={PillListScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <View style={styles.topScreen}>
+        <Text>Top Screen!</Text>
+      </View>
+      <View style={styles.bottomScreen}>
+        <Text>Bottom screen!</Text>
+      </View>
+      <PillList day={"Monday"}/>
+      <StatusBar style="auto" />
+    </View>  
   );
 }
-
-
-
-
-
-
 
 const widthProportion = '100%';
 const heightProportion = '50%';
