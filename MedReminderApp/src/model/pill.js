4e234afc8af5f27fd55c04a7e2b1ac15c9910dac
atienzisaa <atienzisaa@ecs.vuw.ac.nz>
2022-08-19 17:35:30 +1200

    changed lowercase p

--- MedReminderApp/src/model/pill.js
+++ MedReminderApp/src/model/pill.js
@@ -1,7 +1,6 @@
 // TODO: extend Component and implement render() method for pill icon 
 import { Component } from 'react';
 
-
 export default class Pill extends Component{
   constructor(props){
     super(props);
