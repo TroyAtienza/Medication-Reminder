82fd29ea47f45fe8d24012ff9831d9570edf7b04
Johanah Gloria <gloriajoha@myvuw.ac.nz>
2022-08-19 17:44:13 +1200

    Added button, need to render image

--- MedReminderApp/src/screens/03_AddPillColourScreen.js
+++ MedReminderApp/src/screens/03_AddPillColourScreen.js
@@ -1,37 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { PillList } from '../model/PillList';
-import { StyleSheet, Button, Text, Alert, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, Text, Alert, View, TouchableOpacity, Image } from 'react-native';
 import AddButton from '../components/AddButton';
 import { useNavigation } from "@react-navigation/native";
 
 const AddPillColourScreen = (props) => {
   const navigation = useNavigation();
-    return (
-      <View style={styles.container}>
-
-          {/* Top Nav */}
-        <View style={{backgroundColor : "#A3CEF1", height : "15%", width: "100%"}}>
-            <View style={styles.fixToText}>
-              <Button title="Profile"
-              onPress={() => Alert.alert("insert profile link here")}/>
-              <Text style={{color : "#6096BA", fontSize : 30, fontWeight : "bold", textAlign : "center"}}> MedApp </Text>
-              <Button title="Sign out"
-              onPress={() => Alert.alert("You are now signed out")}/>
-            </View>
+  return (
+    <View style={styles.container}>
+      {/* Top Nav */}
+      <View style={{backgroundColor : "#A3CEF1", height : "15%", width: "100%"}}>
+          <View style={styles.fixToText}>
+          <Button title="Profile"
+            onPress={() => Alert.alert("insert profile link here")}/>
+          <Text style={{color : "#6096BA", fontSize : 30, fontWeight : "bold", textAlign : "center"}}> MedApp </Text>
+          <Button title="Sign out"
+            onPress={() => Alert.alert("You are now signed out")}/>
         </View>
-
-        {/* Top Screen */}
-        <View style={styles.topScreen}>
-          <Text>Pill Colour!</Text>
-        </View>
-
-        {/* Bottom Screen */}
-        <View style={styles.bottomScreen}>
-          <Text>Bottom screen!</Text>
-        </View>
-
       </View>
-    );
+
+      {/* Top Screen */}
+      <View style={styles.topScreen}>
+        <Text>Pill Colour!</Text>
+      </View>
+
+      {/* Bottom Screen */}
+        <View style={styles.bottomScreen}>
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+            <Image source={require("../assets/Pill Capsule.png")}/>
+          </TouchableOpacity>
+        </View>
+     
+    </View>
+  );
 }
 
 const widthProportion = '100%';
@@ -57,7 +59,7 @@ const styles = StyleSheet.create({
     paddingRight: paddingLeftRight,
   },
     bottomScreen:{
-    backgroundColor: '#808080',
+    backgroundColor: '#9A9A98',
     width: widthProportion,
     height: heightProportion,
     paddingTop: paddingTopBottom,
@@ -69,6 +71,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     top: "15%",
+  },
+  button: {
+    backgroundColor: '#CAE3F7',
+    width: 60,
+    height: 60,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
   },
 });
 
