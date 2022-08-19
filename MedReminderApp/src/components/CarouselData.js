29ef8f484c9a0813c545a90f57da6f7c4a8e9f51
Ronald Grant <grantrona@myvuw.ac.nz>
2022-08-19 21:55:02 +1200

    Moved CarouselData.js to components folder

--- MedReminderApp/src/components/CarouselData.js
+++ MedReminderApp/src/components/CarouselData.js
@@ -2,37 +2,37 @@ const data = [
     {
         id: 1,
         day: "Monday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
     {
         id: 2,
         day: "Tuesday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
     {
         id: 3,
         day: "Wednesday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
     {
         id: 4,
         day: "Thursday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
     {
         id: 5,
         day: "Friday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
     {
         id: 6,
         day: "Saturday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
     {
         id: 7,
         day: "Sunday",
-        image: require("./assets/favicon.png")
+        image: require("../assets/favicon.png")
     },
 ];
 
