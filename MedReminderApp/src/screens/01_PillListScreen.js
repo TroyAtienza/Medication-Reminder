6f40d0c6ceeb25bfd51954261862982adaec41bd
atienzisaa <atienzisaa@ecs.vuw.ac.nz>
2022-08-23 16:23:37 +1200

    converted class component to functional component to fix navigation issue

--- MedReminderApp/src/screens/01_PillListScreen.js
+++ MedReminderApp/src/screens/01_PillListScreen.js
@@ -5,22 +5,24 @@ import { View } from 'react-native';
 import createStyles from '../view/SplitView'
 import TopNav from '../view/TopNav'
 import PillboxCarousel from "../components/PillboxCarousel";
+import { useNavigation } from "@react-navigation/native";
 
 const styles = createStyles()
 
-export default class PillListScreen extends Component {
-  render(){
-    return (
-      <View style={styles.container}>
-        <TopNav/>
-        <View style={styles.topScreen}>
-          <AddButton onPress={() => navigation.navigate("PillType")}/>
-            <PillboxCarousel/>
-        </View>
-        <View style={styles.bottomScreen}>
-          <PillList day={"Monday"}/>
-        </View>
+const PillListScreen = (props) => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <TopNav/>
+      <View style={styles.topScreen}>
+        <AddButton onPress={() => navigation.navigate("PillType")}/>
+        <PillboxCarousel/>
       </View>
-    );
-  }
+      <View style={styles.bottomScreen}>
+        <PillList day={"Monday"}/>
+      </View>
+    </View>
+  );
 }
+
+export default PillListScreen;
